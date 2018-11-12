2db3b5690ac859df2e8ae26da744353f5d3186fb
visionarioso <davidenrique@visionarioso.com>
2018-11-11 20:46:43 -0500

    Update template. Space for d3 graph

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -5,33 +5,21 @@ var sass = require('gulp-sass');
 gulp.task('sass', function() {
     return gulp.src('./sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-<<<<<<< HEAD
-        .pipe(gulp.dest('../src/frontend/css'))
-=======
+
         .pipe(gulp.dest('../src/css'))
->>>>>>> Template initial commit
         .pipe(browserSync.stream());
 });
 
 gulp.task('serve', gulp.series(gulp.parallel('sass'), function() {
     
     browserSync.init({
-<<<<<<< HEAD
-        server: "../src/frontend/",
-=======
         server: "../src/",
->>>>>>> Template initial commit
         open: false
     });
 
     gulp.watch("./sass/*.scss", gulp.parallel('sass')).on('change', browserSync.reload);
-<<<<<<< HEAD
-    gulp.watch("../src/frontend/*.html").on('change', browserSync.reload);
-    gulp.watch("../src/frontend/js/*.js").on('change', browserSync.reload);
-=======
     gulp.watch("../src/*.html").on('change', browserSync.reload);
     gulp.watch("../src/js/*.js").on('change', browserSync.reload);
->>>>>>> Template initial commit
 }));
 
 gulp.task('default', gulp.parallel('serve'));
